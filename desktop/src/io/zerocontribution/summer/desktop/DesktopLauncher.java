8fcfe4214f96391396221dd0bc425a648a5b0aa7
Rob Zienert <rzienert@bloomhealthco.com>
2014-05-03 14:33:54 -0500

    New default size

--- desktop/src/io/zerocontribution/summer/desktop/DesktopLauncher.java
+++ desktop/src/io/zerocontribution/summer/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import io.zerocontribution.summer.SummerGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
 		new LwjglApplication(new SummerGame(), config);
 	}
 }
