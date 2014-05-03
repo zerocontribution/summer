c10c255ea7ccee6594aa28171ca4b4aab6ff6dd3
Rob Zienert <rzienert@bloomhealthco.com>
2014-05-03 12:37:55 -0500

    initial commit

---
+++ desktop/src/io/zerocontribution/summer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.zerocontribution.summer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.zerocontribution.summer.SummerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SummerGame(), config);
+	}
+}
